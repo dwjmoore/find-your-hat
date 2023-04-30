7c5b61d6737dcf31795237383e1897a97875bcaf
David Moore <dwjmoore@gmail.com>
2023-04-30 02:38:34 +0000

    completed inputPlayerMove method

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ class Field {
 	runGame() {
 		console.log("Welcome to the game. Go find your hat, but don't fall into a hole.")
 		while (this._gameOn) {
-			print();
-			// take in player move and update player location
+			this.print();
+			this.inputPlayerMove();
 			// test player location
 			// update field or end game
+			this._gameOn = false;
 		}
 		console.log("Thank you for playing! Good bye!");
 	}
@@ -30,8 +31,62 @@ class Field {
 		}
 	}
 
-	
+	inputPlayerMove() {
+		let tempLocation = this._playerLocation;
+		const fieldColLength = this._field.length;
+		const fieldRowLength = this._field[0].length
+
+		let playerMove = prompt("What direction to you want to go? Type L, R, U, or D: ");
+		playerMove = playerMove.toUpperCase();
+
+		while (playerMove !== "L" && playerMove !== "R" && playerMove !== "U" && playerMove !== "D") {
+			playerMove = prompt("Invalid entry. Type L, R, U, or D: ");
+			playerMove = playerMove.toUpperCase();
+		}
+
+		if (playerMove === "U") tempLocation[0] -= 1;
+		if (playerMove === "D") tempLocation[0] += 1;
+		if (playerMove === "L") tempLocation[1] -= 1;
+		if (playerMove === "R") tempLocation[1] += 1;
+
+		while (tempLocation[0] < 0 || tempLocation[0] >= fieldColLength || tempLocation[1] < 0 || tempLocation[1] >= fieldRowLength) {
+			if (playerMove === "U") tempLocation[0] += 1;
+			if (playerMove === "D") tempLocation[0] -= 1;
+			if (playerMove === "L") tempLocation[1] += 1;
+			if (playerMove === "R") tempLocation[1] -= 1;
+
+			playerMove = prompt("Out of bounds! Try a different direction: ");
+			playerMove = playerMove.toUpperCase();
+
+			while (playerMove !== "L" && playerMove !== "R" && playerMove !== "U" && playerMove !== "D") {
+				playerMove = prompt("Invalid entry. Type L, R, U, or D: ");
+				playerMove = playerMove.toUpperCase();
+			}
+
+			tempLocation = this._playerLocation;
+			if (playerMove === "U") tempLocation[0] -= 1;
+			if (playerMove === "D") tempLocation[0] += 1;
+			if (playerMove === "L") tempLocation[1] -= 1;
+			if (playerMove === "R") tempLocation[1] += 1;
+		}
+
+		console.log(`Your location is ${this._playerLocation}`);
+	}
+
+
 }
+
+const field = new Field([
+	['*', '░', 'O', '░', '░', '░', '░', '░'],
+	['░', 'O', '░', '░', '░', '░', '░', '░'],
+	['░', '^', '░', '░', '░', '░', '░', '░'],
+	['░', 'O', '░', '░', '░', '░', '░', '░'],
+	['░', 'O', '░', '░', '░', '░', '░', '░'],
+	['░', 'O', '░', '░', '░', '░', '░', '░'],
+	['░', 'O', '░', '░', '░', '░', '░', '░'],
+	['░', 'O', '░', '░', '░', '░', '░', '░'],
+]);
+field.runGame();
 
 /*
 testPlayerLocation() {
